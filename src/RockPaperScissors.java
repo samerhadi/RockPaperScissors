2d04a635e508ae5f485a65fca80c9dc0fa800d26
Samer Hadi <samerhadi1997@hotmail.se>
2023-05-09 22:58:44 +0200

    Full project

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Main {
+public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String s;
